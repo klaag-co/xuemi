0fceea4d3816ed16de11a2e2d59e839e08d11f83
Nylecarg <gracelyn_gosal@s2023.ssts.edu.sg>
2024-04-16 15:57:08 +0800

    YAYYACTUALIOS

---
+++ XuemiiOS/XuemiiOS/XuemiiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  XuemiiOSApp.swift
+//  XuemiiOS
+//
+//  Created by Gracelyn Gosal on 16/4/24.
+//
+
+import SwiftUI
+
+@main
+struct XuemiiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

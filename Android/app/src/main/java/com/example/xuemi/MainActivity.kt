43d00673ac93da061eef06b21e27e8fd63b625d4
abigail <lau_rei_yan_abigail@s2023.ssts.edu.sg>
2024-04-16 16:25:29 +0800

    Update MainActivity.kt

--- Android/app/src/main/java/com/example/xuemi/MainActivity.kt
+++ Android/app/src/main/java/com/example/xuemi/MainActivity.kt
@@ -15,24 +15,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            XuemiTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Main()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Main() {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello!"
     )
 }
 
@@ -40,6 +31,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     XuemiTheme {
-        Greeting("Android")
+        Main()
     }
 }

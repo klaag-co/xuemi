a85e06193dc38135649855862cfc4d13c1ef45ed
abigail <lau_rei_yan_abigail@s2023.ssts.edu.sg>
2024-04-17 11:18:57 +0800

    Update MainActivity.kt
    added a title

--- Android/app/src/main/java/com/example/xuemi/MainActivity.kt
+++ Android/app/src/main/java/com/example/xuemi/MainActivity.kt
@@ -2,13 +2,22 @@ package com.example.xuemi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.xuemi.ui.theme.XuemiTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +31,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Main() {
-    Text(
-        text = "Hello!"
-    )
+    Column {
+        Text(
+            "Home",
+            fontSize = 70.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+        Row {
+            Button(onClick = { /*TODO*/ }) {
+                Column {
+                    Text("ee")
+                    Text("oh wow")
+                }
+            }
+        }
+    }
+    
 }
 
 @Preview(showBackground = true)

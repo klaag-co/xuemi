4158716e7d66fc7c5a78ea6330f908847db56ef0
Nylecarg <gracelyn_gosal@s2023.ssts.edu.sg>
2024-04-23 15:40:09 +0800

    OK

---
+++ XuemiiOS/XuemiiOS/FlashcardView.swift
@@ -0,0 +1,18 @@
+//
+//  FlashcardView.swift
+//  XuemiiOS
+//
+//  Created by Gracelyn Gosal on 22/4/24.
+//
+
+import SwiftUI
+
+struct FlashcardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlashcardView()
+}

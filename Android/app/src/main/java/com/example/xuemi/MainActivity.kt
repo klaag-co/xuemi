e354aff334d1499c70fbe9a068003a255871fb83
abigail <lau_rei_yan_abigail@s2023.ssts.edu.sg>
2024-04-23 14:51:44 +0800

    Update MainActivity.kt
    Redo buttons

--- Android/app/src/main/java/com/example/xuemi/MainActivity.kt
+++ Android/app/src/main/java/com/example/xuemi/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -38,13 +39,27 @@ fun Main() {
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(horizontal = 16.dp)
         )
-        Row {
-            Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(16.dp)) {
-                Text(
-                    "test",
-                    modifier = Modifier.padding(16.dp))
-            }
+        Button(onClick = { /*TODO*/ }) {
+            Image
         }
+//        Row {// 1st button row
+//            Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(16.dp), shape = RoundedCornerShape(20.dp)) {
+//                Column {
+//                    Text(
+//                        text = "Secondary",
+//                        fontSize = 30.sp
+//                    )
+//                    Text(
+//                        text = "1",
+//                        fontSize = 80.sp,
+//                        modifier = Modifier.padding(horizontal = 58.dp,)
+//                    )
+//                }
+//
+//            }
+//
+//
+//        }
     }
     
 }

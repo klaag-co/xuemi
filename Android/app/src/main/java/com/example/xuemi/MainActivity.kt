c3672f368fe2d8bf127d1358d34af4ceb78c77b4
abigail <lau_rei_yan_abigail@s2023.ssts.edu.sg>
2024-04-28 20:44:31 +0800

    Update MainActivity.kt
    Added second row of buttons

--- Android/app/src/main/java/com/example/xuemi/MainActivity.kt
+++ Android/app/src/main/java/com/example/xuemi/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.xuemi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -19,6 +23,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -42,12 +47,17 @@ fun Main() {
     Column {// Whole app Column
         Text(
             "Home",
-            fontSize = 70.sp,
+            fontSize = 65.sp,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(horizontal = 16.dp)
         )
 
-        Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(3,115,206)), shape = RoundedCornerShape(20.dp), modifier = Modifier.padding(horizontal = 16.dp)) {
+        Button(onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(Color(3,115,206)),
+            border = BorderStroke(6.dp, Brush.verticalGradient(listOf(Color.Black, Color.White))),
+            shape = RoundedCornerShape(20.dp),
+            modifier = Modifier.padding(horizontal = 16.dp))
+        {
             Image(
                 painter = painterResource(id = R.drawable.continue_learning),
                 contentDescription = "Continue learning button",
@@ -56,23 +66,25 @@ fun Main() {
             )
         }
         Row {// 1st button row
-            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(126, 190, 240)), modifier = Modifier.padding(horizontal = 22.dp, vertical = 16.dp), shape = RoundedCornerShape(20.dp)) {
+            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(126, 190, 240)), border = BorderStroke(6.dp,Brush.verticalGradient(listOf(Color(90, 142, 179), Color.White))), modifier = Modifier.absolutePadding(top = 14.dp, bottom = 14.dp, right = 12.dp, left = 28.dp), shape = RoundedCornerShape(20.dp)) {
                 Column {
                     Text(
                         text = "Secondary",
-                        fontSize = 24.sp
+                        fontSize = 24.sp,
+
                     )
                     Text(
                         text = "1",
-                        fontSize = 70.sp,
-                        modifier = Modifier.padding(horizontal = 35.dp),
+                        fontSize = 65.sp,
+                        modifier = Modifier
+                            .padding(horizontal = 35.dp)
+                            .absolutePadding(bottom = 10.dp),
                         fontWeight = FontWeight.Black
                     )
-                    Text("")
                 }
 
             }
-            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(126, 190, 240)), modifier = Modifier.padding(horizontal = 1.dp, vertical = 16.dp), shape = RoundedCornerShape(20.dp)) {
+            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(126, 190, 240)), border = BorderStroke(6.dp,Brush.verticalGradient(listOf(Color(90, 142, 179), Color.White))), modifier = Modifier.padding(horizontal = 1.dp, vertical = 14.dp), shape = RoundedCornerShape(20.dp)) {
                 Column {
                     Text(
                         text = "Secondary",
@@ -80,17 +92,75 @@ fun Main() {
                     )
                     Text(
                         text = "2",
-                        fontSize = 70.sp,
-                        modifier = Modifier.padding(horizontal = 35.dp,),
+                        fontSize = 65.sp,
+                        modifier = Modifier
+                            .padding(horizontal = 35.dp)
+                            .absolutePadding(bottom = 10.dp)
+                        ,
                         fontWeight = FontWeight.Black
                     )
-                    Text("")
                 }
 
             }
 
+        }
+        Row {// 2nd button row
+            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(126, 190, 240)), border = BorderStroke(6.dp,Brush.verticalGradient(listOf(Color(90, 142, 179), Color.White))), modifier = Modifier.absolutePadding(right = 12.dp, left = 28.dp), shape = RoundedCornerShape(20.dp)) {
+                Column {
+                    Text(
+                        text = "Secondary",
+                        fontSize = 24.sp
+                    )
+                    Text(
+                        text = "3",
+                        fontSize = 65.sp,
+                        modifier = Modifier
+                            .padding(horizontal = 35.dp)
+                            .absolutePadding(bottom = 10.dp)
+                        ,
+                        fontWeight = FontWeight.Black
+                    )
+                }
+
+            }
+            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(126, 190, 240)), border = BorderStroke(6.dp,Brush.verticalGradient(listOf(Color(90, 142, 179), Color.White))), modifier = Modifier.padding(horizontal = 1.dp), shape = RoundedCornerShape(20.dp)) {
+                Column {
+                    Text(
+                        text = "Secondary",
+                        fontSize = 24.sp
+                    )
+                    Text(
+                        text = "4",
+                        fontSize = 65.sp,
+                        modifier = Modifier
+                            .padding(horizontal = 35.dp)
+                            .absolutePadding(bottom = 10.dp)
+                        ,
+                        fontWeight = FontWeight.Black
+                    )
+                }
+
+            }
 
         }
+//        Button(onClick = { /*TODO*/ },
+//            colors = ButtonDefaults.buttonColors(Color(126, 190, 240)),
+//            border = BorderStroke(6.dp, Brush.verticalGradient(listOf(Color(90, 142, 179), Color.White))),
+//            shape = RoundedCornerShape(20.dp),
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(horizontal = 22.dp, vertical = 8.dp))
+//
+//
+//        {
+//            Text(
+//                text = "O-Level\n\nPractice",
+//                fontSize = 40.sp,
+//            )
+//        }
+//        BottomAppBar {
+//
+//        }
     }
     
 }

9469084b79751217b81ae2dff62a113a7cd9219d
Nylecarg <gracelyn_gosal@s2023.ssts.edu.sg>
2024-04-29 21:01:25 +0800

    update
    yes

--- XuemiiOS/XuemiiOS/SOneFlashcardView.swift
+++ XuemiiOS/XuemiiOS/SOneFlashcardView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct FlashcardView: View {
+struct SOneFlashcardView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    FlashcardView()
+    SOneFlashcardView()
 }

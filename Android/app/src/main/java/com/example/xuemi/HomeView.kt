14976f38c9377edbda79f6af2902c07d89f5eac4
abigail <lau_rei_yan_abigail@s2023.ssts.edu.sg>
2024-04-30 07:20:36 +0800

    Revert "Add and Update MainActivity.kt"
    This reverts commit 55c10cce5f3eec04c568a6900052c9ffd7a20512.

--- Android/app/src/main/java/com/example/xuemi/HomeView.kt
+++ Android/app/src/main/java/com/example/xuemi/HomeView.kt
@@ -1,8 +1,10 @@
 package com.example.xuemi
-
-import android.net.wifi.hotspot2.pps.HomeSp
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.absolutePadding
@@ -10,20 +12,47 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.xuemi.ui.theme.XuemiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainBottomBar()
+        }
+    }
+}
 
 @Composable
-fun Home() {
+fun Main() {
+
+
     Column {// Whole app Column
         Text(
             "Home",
@@ -46,15 +75,13 @@ fun Home() {
             )
         }
         Row {// 1st button row
-            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(126, 190, 240)), border = BorderStroke(6.dp,
-                Brush.verticalGradient(listOf(Color(90, 142, 179), Color.White))), modifier = Modifier.absolutePadding(top = 14.dp, bottom = 14.dp, right = 12.dp, left = 28.dp), shape = RoundedCornerShape(20.dp)
-            ) {
+            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(126, 190, 240)), border = BorderStroke(6.dp,Brush.verticalGradient(listOf(Color(90, 142, 179), Color.White))), modifier = Modifier.absolutePadding(top = 14.dp, bottom = 14.dp, right = 12.dp, left = 28.dp), shape = RoundedCornerShape(20.dp)) {
                 Column {
                     Text(
                         text = "Secondary",
                         fontSize = 24.sp,
 
-                        )
+                    )
                     Text(
                         text = "1",
                         fontSize = 65.sp,
@@ -66,9 +93,7 @@ fun Home() {
                 }
 
             }
-            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(126, 190, 240)), border = BorderStroke(6.dp,
-                Brush.verticalGradient(listOf(Color(90, 142, 179), Color.White))), modifier = Modifier.padding(horizontal = 1.dp, vertical = 14.dp), shape = RoundedCornerShape(20.dp)
-            ) {
+            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(126, 190, 240)), border = BorderStroke(6.dp,Brush.verticalGradient(listOf(Color(90, 142, 179), Color.White))), modifier = Modifier.padding(horizontal = 1.dp, vertical = 14.dp), shape = RoundedCornerShape(20.dp)) {
                 Column {
                     Text(
                         text = "Secondary",
@@ -89,9 +114,7 @@ fun Home() {
 
         }
         Row {// 2nd button row
-            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(126, 190, 240)), border = BorderStroke(6.dp,
-                Brush.verticalGradient(listOf(Color(90, 142, 179), Color.White))), modifier = Modifier.absolutePadding(right = 12.dp, left = 28.dp), shape = RoundedCornerShape(20.dp)
-            ) {
+            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(126, 190, 240)), border = BorderStroke(6.dp,Brush.verticalGradient(listOf(Color(90, 142, 179), Color.White))), modifier = Modifier.absolutePadding(right = 12.dp, left = 28.dp), shape = RoundedCornerShape(20.dp)) {
                 Column {
                     Text(
                         text = "Secondary",
@@ -109,9 +132,7 @@ fun Home() {
                 }
 
             }
-            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(126, 190, 240)), border = BorderStroke(6.dp,
-                Brush.verticalGradient(listOf(Color(90, 142, 179), Color.White))), modifier = Modifier.padding(horizontal = 1.dp), shape = RoundedCornerShape(20.dp)
-            ) {
+            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(126, 190, 240)), border = BorderStroke(6.dp,Brush.verticalGradient(listOf(Color(90, 142, 179), Color.White))), modifier = Modifier.padding(horizontal = 1.dp), shape = RoundedCornerShape(20.dp)) {
                 Column {
                     Text(
                         text = "Secondary",
@@ -137,7 +158,7 @@ fun Home() {
             shape = RoundedCornerShape(20.dp),
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 25.dp, vertical = 6.dp))
+                .padding(horizontal = 25.dp, vertical = 8.dp))
 
 
         {
@@ -151,5 +172,45 @@ fun Home() {
 
 
     }
+    
+}
+@Composable
+fun MainBottomBar() {
+    var currentpage by remember { mutableStateOf("Home") }
 
+
+    if (currentpage == "Home") {
+        Main()
+    } else if (currentpage == "Favourites") {
+        Favourites()
+    } else if (currentpage == "Notes") {
+        Notes()
+    } else if (currentpage == "Settings") {
+        /*TODO*/
+    }
+
+    BottomAppBar (
+        actions = {
+            IconButton(onClick = { currentpage = "Home"}) {
+                Icon(imageVector = Icons.Outlined.Home, contentDescription = null)
+            }
+            IconButton(onClick = { currentpage = "Favourites" }) {
+                Icon(imageVector = Icons.Outlined.Star, contentDescription = null)
+            }
+            IconButton(onClick = { currentpage = "Notes" }) {
+                Icon(imageVector = Icons.Outlined.Menu, contentDescription = null)
+            }
+            IconButton(onClick = { currentpage = "Settings" }) {
+                Icon(imageVector = Icons.Outlined.Settings, contentDescription = null)
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    XuemiTheme {
+        MainBottomBar()
+    }
 }

ef11dbce610c1ddb8b4be2459ed151043ea5c22d
Nylecarg <gracelyn_gosal@s2023.ssts.edu.sg>
2024-05-30 20:18:58 +0800

    noteview!!!!!!!
    TRISTAN CARRY

--- XuemiiOS/XuemiiOS/NotesView.swift
+++ XuemiiOS/XuemiiOS/NotesView.swift
@@ -2,14 +2,67 @@
 //  NotesView.swift
 //  XuemiiOS
 //
-//  Created by Gracelyn Gosal on 16/4/24.
+//  Created by Gracelyn Gosal on 30/5/24.
 //
 
 import SwiftUI
 
 struct NotesView: View {
+    
+    @State private var isCreateNoteViewPresented = false
+    @ObservedObject var notesManager: NotesManager = .shared
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section {
+                    ForEach($notesManager.notes.filter({$0.noteType.wrappedValue == NoteType.exam}), id: \.id) { $note in
+                        NavigationLink {
+                            NotesDetailView(note: $note)
+                        } label: {
+                            Text(note.title)
+                        }
+                    }
+                    .onDelete { indexSet in
+                        notesManager.notes.remove(atOffsets: indexSet)
+                    }
+                } header: {
+                    Text("Exam")
+                }
+                
+                Section {
+                    ForEach($notesManager.notes.filter({$0.noteType.wrappedValue == NoteType.note}), id: \.id) { $note in
+                        NavigationLink {
+                            NotesDetailView(note: $note)
+                        } label: {
+                            Text(note.title)
+                        }
+                    }
+                    .onDelete { indexSet in
+                        notesManager.notes.remove(atOffsets: indexSet)
+                    }
+                } header: {
+                    Text("Notes")
+                }
+            }
+            .navigationTitle("Notepad")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        isCreateNoteViewPresented.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+            .sheet(isPresented: $isCreateNoteViewPresented) {
+                CreateNoteView()
+            }
+        }
     }
 }
 

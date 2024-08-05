d0676fb5bf059ad6f625fec91e2c7b4762f75253
kmy <kmy_er_sze_lei@s2023.ssts.edu.sg>
2024-08-05 17:26:38 +0800

    Update SettingsView.kt

--- Android/app/src/main/java/com/example/xuemi/SettingsView.kt
+++ Android/app/src/main/java/com/example/xuemi/SettingsView.kt
@@ -1,18 +1,40 @@
 package com.example.xuemi
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Preview(showSystemUi = true)
+@Composable
+fun Person() {
+    Card {
+        Row {
+            Column {
+                Text(
+                    "Title",
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    "Description"
+                )
+            }
+            Icon(painter = painterResource(id = R.drawable.home), contentDescription = "icon")
+        }
+    }
+}
+
 @Composable
 fun Settings() {
     LazyColumn {
@@ -22,19 +44,9 @@ fun Settings() {
                 "Settings",
                 fontSize = 100.sp,
                 fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(horizontal = 16.dp)
             )
         }
-        item {
-            Text(
-                text = "APP",
-                modifier = Modifier.padding(horizontal = 30.dp),
-                color = Color.Gray
-            )
-        }
-        items(9) {
-            Card {}
-        }
-
     }
 }
+
+   

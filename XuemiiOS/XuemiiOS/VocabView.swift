a075acf9e23122abd4bccba32d56309c1edeeffe
Nylecarg <gracelyn_gosal@s2023.ssts.edu.sg>
2024-11-12 23:42:52 +0800

    vocab & settings

---
+++ XuemiiOS/XuemiiOS/VocabView.swift
@@ -0,0 +1,18 @@
+//
+//  VocabView.swift
+//  XuemiiOS
+//
+//  Created by Gracelyn Gosal on 12/11/24.
+//
+
+import SwiftUI
+
+struct VocabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VocabView()
+}
